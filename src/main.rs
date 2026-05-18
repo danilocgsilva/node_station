855b6f9a1d8f259666c4290242e628e0aa44d7cf
Danilo Silva <danilocgsilva@gmail.com>
2026-05-18 19:54:51 -0300

    Loop
    To simulate time by time monitor

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    loop {
+        println!("This will print every second.");
+
+        thread::sleep(Duration::from_secs(1));
+    }
+}
